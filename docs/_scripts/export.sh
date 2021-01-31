c3a6b5d3fec6a747a43e7aace77fb3a424dbfb74
David Davis <daviddavis@redhat.com>
2021-01-31 11:52:05 -0500

    Update workflow docs to use pulp-cli
    fixes #7530

--- docs/_scripts/export.sh
+++ docs/_scripts/export.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
+
+# TODO: convert file to pulp-cli once the filesystem exporter commands are implemented
+
 export TASK_URL=$(http POST $BASE_ADDR$EXPORTER_HREF'exports/' publication=$PUBLICATION_HREF \
   | jq -r '.task')
 
-# Poll the task (here we use a function defined in docs/_scripts/base.sh)
-wait_until_task_finished $BASE_ADDR$TASK_URL
+# Poll the task
+while true
+do
+  case $(http $BASE_ADDR$TASK_URL | jq -r .state) in
+    failed|canceled)
+      echo "Task in final state: ${state}"
+      exit 1
+      ;;
+    completed)
+      echo "$task_url complete."
+      break
+      ;;
+    *)
+      echo "Still waiting..."
+      sleep 1
+      ;;
+  esac
+done
 
 echo "Inspecting export at $DEST_DIR"
 ls $DEST_DIR
